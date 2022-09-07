1f0fe27963237945bd9bf31b5706cf47d3c6e358
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-09-06 21:40:22 -0500

    add: views details, results and votes

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index-test")
+    #ex: polls
+    path("", views.index, name="index"),
+    #ex: polls/5/detail
+    path("details/<int:question_id>/", views.details, name="detail"),
+    #ex: polls/5/results
+    path("results/<int:question_id>/", views.results, name="result"),
+    #ex: polls/5/vote
+    path("vote/<int:question_id>/", views.votes, name="")
 ]
--- polls_app/polls/views.py
+++ polls_app/polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("HELLO FUCKING WORLD !!!")
+    return HttpResponse("<h1>polls app index</h1>")
+
+
+def details(request, question_id):
+    return HttpResponse(f"This is the question number: {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"This is the Results of the question number: {question_id}")
+
+
+def votes(request, question_id):
+    return HttpResponse(f"You arte voting for question number: {question_id}")
