e46372f16fac310e3fb426d1d7808b137455f0b0
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-09-08 20:22:45 -0500

    add: templates details, index, result and votes

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -2,13 +2,14 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     #ex: polls
     path("", views.index, name="index"),
     #ex: polls/5/detail
-    path("details/<int:question_id>/", views.details, name="detail"),
+    path("details/<int:question_id>/", views.details, name="details"),
     #ex: polls/5/results
-    path("results/<int:question_id>/", views.results, name="result"),
+    path("results/<int:question_id>/", views.results, name="results"),
     #ex: polls/5/vote
-    path("vote/<int:question_id>/", views.votes, name="")
+    path("vote/<int:question_id>/", views.votes, name="votes")
 ]
--- polls_app/polls/views.py
+++ polls_app/polls/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("<h1>polls app index</h1>")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list,
+    })
 
 
 def details(request, question_id):
-    return HttpResponse(f"This is the question number: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    print(question)
+    return render(request, "polls/details.html", {
+        "question": question,
+    })
 
 
 def results(request, question_id):
