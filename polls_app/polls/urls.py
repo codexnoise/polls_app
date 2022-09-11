03baaf7ea0dbab4adf86699df08b3d3c698e440b
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-09-10 22:23:50 -0500

    change: views details and results of function based views to generic views

--- polls_app/polls/urls.py
+++ polls_app/polls/urls.py
@@ -5,11 +5,13 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     #ex: polls
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     #ex: polls/5/detail
-    path("details/<int:question_id>/", views.details, name="details"),
+    path("details/<int:pk>/",
+         views.DetailsView.as_view(), name="details"),
     #ex: polls/5/results
-    path("results/<int:question_id>/", views.results, name="results"),
+    path("results/<int:pk>/",
+         views.ResultsView.as_view(), name="results"),
     #ex: polls/5/vote
     path("vote/<int:question_id>/", views.votes, name="votes")
 ]
