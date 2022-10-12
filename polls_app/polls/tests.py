7aa058feffa51d9f9d6b8b8086c7fd2d9a011465
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-10-11 22:33:01 -0500

    add method QuestionModelTest

--- polls_app/polls/tests.py
+++ polls_app/polls/tests.py
@@ -1,3 +1,16 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+# TEST OF MODELS
+
+
+class QuestionModelTest(TestCase):
+    def test_was_publish_recently_with_future_questions(self):
+        """"was_publish_recently() retuns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(
+            question_text="Quien es el mejor CD of Platzi", pub_date=time)
+        self.assertIs(future_question.was_publish_recently(), False)
